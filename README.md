3e13cbfbad6562c56cf21d73082ec3e718a3e9ac
baoxiao_tong <baoxiao_tong@hotmail.com>
2018-06-29 16:59:20 +0800

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# wendong2048.github.io
-2048 game the first commit
+# My2048
+<p> This is a web  2048 game</p>
+<p>Gameplay: use up and down to control the game.</p>
+
+UI show
+![Image text](https://github.com/xiaobooo/img_buffer/blob/master/img/my2018.png)
---
+++ js/loading.js
@@ -0,0 +1,36 @@
+// imitation of new page loading
+function loading() {
+  $body = $('body'),
+  $('.loading')
+
+  loader(10);
+
+  function loader(delay) {
+  	$('.loading').css('visibility','visible')
+    setTimeout(function(){
+      $body.addClass('loading');
+    }, delay);
+
+    setTimeout(function(){
+      $body.addClass('loaded');
+    }, delay + 1700);
+
+    setTimeout(function(){
+      $body.removeClass('restart').addClass('new-page');
+    }, delay + 1950);
+
+    setTimeout(function(){
+    console.log('hi')
+      $body.css('background-color','#FFF')
+      $('#c').css('display', 'none')
+    }, delay + 2150);
+
+
+
+
+  }
+  
+  function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
